93f83674bd8a12b61c071c7fe2f8a4df1c0c7b5d
Saumya <saumyathakurr1311@gmail.com>
2026-03-07 04:06:24 +0530

    Time: 33 ms (93.37%) | Memory: 91.3 MB (86.19%) - LeetSync

--- 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.java
+++ 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.java
@@ -1,46 +1,44 @@
-import java.util.*;
-
 class Solution {
-    private int check(int[] nums, int n, int t, int m) {
-        long nL = n;
-        long tL = t;
-        int l = lowerBound(nums, nL);
-        int h = upperBound(nums, nL);
-        int ll = lowerBound(nums, nL - tL);
-        int hh = upperBound(nums, nL + tL);
-        int res = (hh - h) + (l - ll);
-        return Math.min(m, res) + (h - l);
-    }
-
-    private int lowerBound(int[] arr, long target) {
-        int l = 0, r = arr.length;
-        while (l < r) {
-            int mid = (l + r) / 2;
-            if (arr[mid] < target) l = mid + 1;
-            else r = mid;
-        }
-        return l;
-    }
-
-    private int upperBound(int[] arr, long target) {
-        int l = 0, r = arr.length;
-        while (l < r) {
-            int mid = (l + r) / 2;
-            if (arr[mid] <= target) l = mid + 1;
-            else r = mid;
-        }
-        return l;
-    }
-
     public int maxFrequency(int[] nums, int k, int numOperations) {
-        int m = numOperations;
         Arrays.sort(nums);
-        int ans = 1;
-        for (int i = 0; i < nums.length - 1; i++) {
-            ans = Math.max(ans, check(nums, nums[i], k, m));
-            ans = Math.max(ans, check(nums, nums[i] - k, k, m));
-            ans = Math.max(ans, check(nums, nums[i] + k, k, m));
+        int n = nums.length;
+
+        int left = 0;
+        int right = 0;
+        int sumCount = 0;
+        int result = 0;
+        int left2 = 0;
+        int sumCount2 = 0;
+        int count = 0;
+        int prev = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            if (num == prev) {
+                count++;
+            } else {
+                prev = num;
+                count = 1;
+            }
+
+            while (nums[left] < num - k) {
+                sumCount--;
+                left++;
+            }
+
+            while (right < n && nums[right] <= num + k) {
+                sumCount++;
+                right++;
+            }
+            result = Math.max(result, count + Math.min(sumCount - count, numOperations));
+
+            sumCount2++;
+            while (nums[left2] < num - 2L * k) {
+                sumCount2--;
+                left2++;
+            }
+            result = Math.max(result, Math.min(sumCount2, numOperations));
         }
-        return ans;
+
+        return result;
     }
 }

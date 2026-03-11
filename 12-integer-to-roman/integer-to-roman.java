e6fc2175b759ede9afdf617f5153f25316ce19a6
Saumya <saumyathakurr1311@gmail.com>
2026-03-11 23:54:44 +0530

    Time: 7 ms (12.94%) | Memory: 47.2 MB (6.62%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,9 +1,28 @@
 class Solution {
-    public static String intToRoman(int num) {
-        String M[] = {"", "M", "MM", "MMM"};
-        String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-        String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-        String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-        return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+    public String intToRoman(int num) {
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "I");
+        map.put(4, "IV");
+        map.put(5, "V");
+        map.put(9, "IX");
+        map.put(10, "X");
+        map.put(40, "XL");
+        map.put(50, "L");
+        map.put(90, "XC");
+        map.put(100, "C");
+        map.put(400, "CD");
+        map.put(500, "D");
+        map.put(900, "CM");
+        map.put(1000, "M");
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String result = "";
+        for(int v : values){
+            while(num >= v){
+                result += map.get(v);
+                num -= v;
+            }
+        }
+        return result;
     }
 }
+    

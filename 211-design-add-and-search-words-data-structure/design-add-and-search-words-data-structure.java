6005bb4756f4d3289ac668389c9f86fbd40c1d45
Saumya <saumyathakurr1311@gmail.com>
2026-03-13 04:28:50 +0530

    Time: 272 ms (23.57%) | Memory: 277.6 MB (21.63%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.java
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.java
@@ -1,16 +1,14 @@
-public class TrieNode {
-
-    TrieNode[] children;
-    boolean word;
-
+class TrieNode {
+    Map<Character, TrieNode> children;
+    boolean isWord;
+    
     public TrieNode() {
-        children = new TrieNode[26];
-        word = false;
+        children = new HashMap<>();
+        isWord = false;
     }
 }
 
 class WordDictionary {
-
     private TrieNode root;
 
     public WordDictionary() {
@@ -18,39 +16,54 @@ class WordDictionary {
     }
 
     public void addWord(String word) {
-        TrieNode cur = root;
-        for (char c : word.toCharArray()) {
-            if (cur.children[c - 'a'] == null) {
-                cur.children[c - 'a'] = new TrieNode();
+        TrieNode node = root;
+        // Traverse the trie for each character in the word
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            // If the current node does not have a child with character c,
+            // create a new node and add it as a child of the current node
+            if (!node.children.containsKey(c)) {
+                node.children.put(c, new TrieNode());
             }
-            cur = cur.children[c - 'a'];
+            // Move to the child node corresponding to character c
+            node = node.children.get(c);
         }
-        cur.word = true;
+        // Mark the last node as a word node
+        node.isWord = true;
     }
 
     public boolean search(String word) {
-        return dfs(word, 0, root);
+        return searchHelper(root, word, 0);
     }
 
-    private boolean dfs(String word, int j, TrieNode root) {
-        TrieNode cur = root;
-
-        for (int i = j; i < word.length(); i++) {
-            char c = word.charAt(i);
-            if (c == '.') {
-                for (TrieNode child : cur.children) {
-                    if (child != null && dfs(word, i + 1, child)) {
-                        return true;
-                    }
-                }
-                return false;
-            } else {
-                if (cur.children[c - 'a'] == null) {
-                    return false;
-                }
-                cur = cur.children[c - 'a'];
-            }
+    private boolean searchHelper(TrieNode node, String word, int index) {
+        // If we have reached the end of the word,
+        // check if the current node is a word node
+        if (index == word.length()) {
+            return node.isWord;
         }
-        return cur.word;
+        char c = word.charAt(index);
+        if (c == '.') {
+            // If the current character is a dot, we need to check all children of the current node
+            // recursively by skipping over the dot character and moving to the next character of the word
+            for (TrieNode child : node.children.values()) {
+                if (searchHelper(child, word, index + 1)) {
+                    return true;
+                }
+            }
+            // If no child node matches the remaining characters of the word,
+            // return false
+            return false;
+        } else {
+            // If the current character is not a dot, move to the child node
+            // corresponding to that character and continue recursively
+            TrieNode child = node.children.get(c);
+            if (child == null) {
+                // If there is no child node corresponding to the current character,
+                // return false
+                return false;
+            }
+            return searchHelper(child, word, index + 1);
+        }
     }
 }

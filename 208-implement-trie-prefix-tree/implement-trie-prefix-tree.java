2daa96d3e8fdd3f38cbb2a17a0ca158ba135f613
Saumya <saumyathakurr1311@gmail.com>
2026-03-13 04:25:13 +0530

    Time: 43 ms (19.06%) | Memory: 63.8 MB (7.00%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,31 +1,6 @@
-class TrieNode {
-    private TrieNode[] links;
-    private final int R = 26;
-    private boolean isEnd;
-
-    public TrieNode(){
-        links = new TrieNode[26];
-    }
-
-    public boolean containsKey(char ch){
-        return links[ch - 'a'] != null;
-    }
-
-    public TrieNode get(char ch){
-        return links[ch - 'a'];
-    }
-
-    public void put(char ch, TrieNode node){
-        links[ch - 'a'] = node;
-    }
-
-    public void setEnd(){
-        isEnd = true;
-    }
-
-    public boolean isEnd(){
-        return isEnd;
-    }
+public class TrieNode {
+    HashMap<Character, TrieNode> children = new HashMap<>();
+    boolean endOfWord = false;
 }
 
 class Trie {
@@ -36,44 +11,33 @@ class Trie {
     }
 
     public void insert(String word) {
-        TrieNode node = root;
-
-        for(int i = 0; i < word.length(); i++){
-            char currchar = word.charAt(i);
-
-            if(!node.containsKey(currchar)){
-                node.put(currchar, new TrieNode());
-            }
-
-            node = node.get(currchar);
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            cur.children.putIfAbsent(c, new TrieNode());
+            cur = cur.children.get(c);
         }
-
-        node.setEnd();
-    }
-
-    public TrieNode searchPre(String word){
-        TrieNode node = root;
-
-        for(int i = 0; i < word.length(); i++){
-            char currchar = word.charAt(i);
-
-            if(node.containsKey(currchar)){
-                node = node.get(currchar);
-            } else {
-                return null;
-            }
-        }
-
-        return node;
+        cur.endOfWord = true;
     }
 
     public boolean search(String word) {
-        TrieNode node = searchPre(word);
-        return node != null && node.isEnd();
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            if (!cur.children.containsKey(c)) {
+                return false;
+            }
+            cur = cur.children.get(c);
+        }
+        return cur.endOfWord;
     }
 
     public boolean startsWith(String prefix) {
-        TrieNode node = searchPre(prefix);
-        return node != null;
+        TrieNode cur = root;
+        for (char c : prefix.toCharArray()) {
+            if (!cur.children.containsKey(c)) {
+                return false;
+            }
+            cur = cur.children.get(c);
+        }
+        return true;
     }
 }

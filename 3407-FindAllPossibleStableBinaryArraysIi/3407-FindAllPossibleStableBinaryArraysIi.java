3d504c3619605a19e5c80cfc6cdc42cb1392e000
Saumya <saumyathakurr1311@gmail.com>
2026-03-14 00:05:02 +0530

    Create 3407-FindAllPossibleStableBinaryArraysIi

---
+++ 3407-FindAllPossibleStableBinaryArraysIi/3407-FindAllPossibleStableBinaryArraysIi.java
@@ -0,0 +1,29 @@
+// Last updated: 3/14/2026, 12:05:03 AM
+class Solution {
+    public int numberOfStableArrays(int zero, int one, int limit) {
+        int MOD = 1_000_000_007;
+        
+        // dp[i][j][k]: ways to fill i zeros, j ones, last element = k (0 or 1)
+        long[][][] dp = new long[zero + 1][one + 1][2];
+        
+        // Base cases
+        for (int i = 1; i <= Math.min(zero, limit); i++) dp[i][0][0] = 1;
+        for (int j = 1; j <= Math.min(one, limit); j++)  dp[0][j][1] = 1;
+        
+        for (int i = 1; i <= zero; i++) {
+            for (int j = 1; j <= one; j++) {
+                // Append 0
+                dp[i][j][0] = (dp[i-1][j][0] + dp[i-1][j][1]) % MOD;
+                if (i - limit - 1 >= 0)
+                    dp[i][j][0] = (dp[i][j][0] - dp[i-limit-1][j][1] + MOD) % MOD;
+                
+                // Append 1
+                dp[i][j][1] = (dp[i][j-1][0] + dp[i][j-1][1]) % MOD;
+                if (j - limit - 1 >= 0)
+                    dp[i][j][1] = (dp[i][j][1] - dp[i][j-limit-1][0] + MOD) % MOD;
+            }
+        }
+        
+        return (int)((dp[zero][one][0] + dp[zero][one][1]) % MOD);
+    }
+}

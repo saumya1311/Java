3660e4deb7251c808bc9fb2e95f3fa26e3dbceda
Saumya <saumyathakurr1311@gmail.com>
2026-03-14 00:05:16 +0530

    Create 907-KokoEatingBananas

---
+++ 907-KokoEatingBananas/907-KokoEatingBananas.java
@@ -0,0 +1,22 @@
+// Last updated: 3/14/2026, 12:05:17 AM
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int l=1;
+        int r=Arrays.stream(piles).max().getAsInt();
+
+        int res=r;
+
+        while(l<=r){
+            int k=l+(r-l)/2;
+            long times=0;
+            for(int p:piles){
+                times+=Math.ceil((double)p/k);
+                }
+            if(times<=h){
+                res=k;
+                r=k-1;
+            } else l=k+1;
+        }
+        return res;
+    }
+}

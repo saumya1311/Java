fb1e32b75b4f9c229247db14abfac53a568bb4e2
Saumya <saumyathakurr1311@gmail.com>
2026-03-14 00:06:21 +0530

    Create 79-WordSearch

---
+++ 79-WordSearch/79-WordSearch.java
@@ -0,0 +1,40 @@
+// Last updated: 3/14/2026, 12:06:22 AM
+class Solution {
+    int[] row_dif={0,-1,0,1};
+    int[] col_dif={-1,0,1,0};
+    boolean is_exist=false;
+    public void rec(char[][] B,String word,int R,int C,int row,int col,int wind,int WLEN){
+        if(wind==WLEN){
+            is_exist=true;
+            return;
+        }
+        char temp=B[row][col];
+        B[row][col]='?';
+        for(int i=0;i<4;i++){
+            int trow=row-row_dif[i];
+            int tcol=col-col_dif[i];
+            if(trow<0 || trow>=R || tcol<0 || tcol>=C)continue;
+            
+            if(B[trow][tcol]==word.charAt(wind)){
+                rec(B,word,R,C,trow,tcol,wind+1,WLEN);
+            }
+            
+
+        }
+        B[row][col]=temp;
+    }
+    public boolean exist(char[][] board, String word) {
+        int R=board.length,C=board[0].length,WLEN=word.length();
+        for(int row=0;row<R;row++){
+            for(int col=0;col<C;col++){
+                if(board[row][col]==word.charAt(0)){
+                    rec(board,word,R,C,row,col,1,WLEN);
+                    if(is_exist){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}

b8f9ba88866b4043d4525f961f77e039c699ad9d
Saumya <saumyathakurr1311@gmail.com>
2026-03-13 05:33:34 +0530

    Time: 150 ms (39.48%) | Memory: 43.1 MB (58.07%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,32 +1,39 @@
 class Solution {
-    boolean visited[][];
+    int[] row_dif={0,-1,0,1};
+    int[] col_dif={-1,0,1,0};
+    boolean is_exist=false;
+    public void rec(char[][] B,String word,int R,int C,int row,int col,int wind,int WLEN){
+        if(wind==WLEN){
+            is_exist=true;
+            return;
+        }
+        char temp=B[row][col];
+        B[row][col]='?';
+        for(int i=0;i<4;i++){
+            int trow=row-row_dif[i];
+            int tcol=col-col_dif[i];
+            if(trow<0 || trow>=R || tcol<0 || tcol>=C)continue;
+            
+            if(B[trow][tcol]==word.charAt(wind)){
+                rec(B,word,R,C,trow,tcol,wind+1,WLEN);
+            }
+            
+
+        }
+        B[row][col]=temp;
+    }
     public boolean exist(char[][] board, String word) {
-        int rows= board.length;
-        int cols=board[0].length;
-
-        visited=new boolean[rows][cols];
-
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(word.charAt(0)==board[i][j] && searchword(i,j,0,word,board)){
-                    return true;
+        int R=board.length,C=board[0].length,WLEN=word.length();
+        for(int row=0;row<R;row++){
+            for(int col=0;col<C;col++){
+                if(board[row][col]==word.charAt(0)){
+                    rec(board,word,R,C,row,col,1,WLEN);
+                    if(is_exist){
+                        return true;
+                    }
                 }
             }
         }
-        return false;
-    }
-
-    public boolean searchword(int i, int j, int index, String word, char[][] board ){
-        if(index==word.length()) return true;
-        if(i<0 || i>=board.length|| j<0 || j>=board[i].length || word.charAt(index)!=board[i][j] || visited[i][j]){
-            return false;
-        }
-
-        visited[i][j]=true;
-        if(searchword(i+1,j,index+1,word,board)|| searchword(i-1,j,index+1,word,board)|| searchword(i,j+1,index+1,word,board)||searchword(i,j-1,index+1,word,board)) return true;
-        
-
-        visited[i][j]=false;
         return false;
     }
 }

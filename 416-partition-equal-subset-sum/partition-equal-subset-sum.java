270c0685042d9d0980a06fc4533f5292253c2645
Saumya <saumyathakurr1311@gmail.com>
2026-04-19 00:48:35 +0530

    Time: 48 ms (52.75%) | Memory: 47.4 MB (58.70%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum =0;
+        int n= nums.length;
+
+        for (int i=0; i<n; i++){
+            sum+=nums[i];
+        }
+
+        if(sum%2!=0) return false;
+
+        int m=sum/2;
+        boolean [][] t= new  boolean [n+1][m+1];
+
+        for (int i = 0; i <= n; i++) {
+            t[i][0] = true;
+        }
+
+        for (int j = 1; j <= m; j++) {
+            t[0][j] = false;
+        }  
+        
+        for(int i =1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(nums[i-1]<=j){
+                    t[i][j]= t[i - 1][j] || t[i - 1][j - nums[i - 1]];
+                }
+                else{
+                    t[i][j]= t[i-1][j];
+                }
+            }
+        }
+        return t[n][m];
+    }
+}

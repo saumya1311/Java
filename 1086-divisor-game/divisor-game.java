2d9ff72a8a15ed56bb01e665f3559a333ccb825b
Saumya <saumyathakurr1311@gmail.com>
2026-04-20 23:31:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (7.27%) - LeetSync

--- 1086-divisor-game/divisor-game.java
+++ 1086-divisor-game/divisor-game.java
@@ -1,23 +1,5 @@
-  class Solution {
-   public boolean divisorGame(int N) {           
-
-        boolean[] cache = new boolean[N+1];
-        if (N== 1){
-            cache[1] = false;
-            return false;
-        }
-       
-        if (N== 2){
-            cache[2] = true;
-            return true;
-        }
-       
-        for (int i=1;i*i<=N;i++){
-            if (N % i == 0){
-                cache[i] = !(divisorGame(N-i));
-                return cache[i];
-            }
-        }
-        return false;
-    }   
- }
+class Solution {
+    public boolean divisorGame(int n) {
+        return n % 2 == 0;
+    }
+}
